f11c3e04c0f7803433e60931f11145662dacecda
David <dagoma0033@gmail.com>
2023-03-04 16:24:20 +0100

    Fix import bug

--- src/chat/hooks.ts
+++ src/chat/hooks.ts
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { loadGroups } from '../redux/chat/thunks';
+import { reloadGroups } from '../redux/chat/thunks';
 import { useAppDispatch } from '../redux/hooks';
 
 //loads group on start
 export const useLoadGroups = () => {
 	const dispatch = useAppDispatch();
 	useEffect(() => {
-		dispatch(loadGroups());
+		dispatch(reloadGroups());
 	}, []);
 };
