9f1797324df2c0f0ae59cf4edb8fa4b687309e94
David <dagoma0033@gmail.com>
2023-03-04 17:48:31 +0100

    Bug fixes, add responsiveness to SideBar and add new icon

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -9,21 +9,23 @@ import { MessageBox } from './MessageBox';
 export const MainContent = () => {
 	const chatInfo = useAppSelector((state: RootState) => state.chat);
 
-	if (!chatInfo.activeGroup) {
-		return <NoGroup />;
-	}
-
 	return (
-		<div className='bg-zinc-200  flex-grow flex flex-col justify-between h-full overflow-x-hidden '>
-			<GroupHeader />
-			<div className='flex-grow overflow-y-auto overflow-x-hidden'>
-				{chatInfo.activeGroup.messages.map(
-					(message: Message, index: number) => (
-						<MessageBox message={message} key={index} />
-					)
-				)}
-			</div>
-			<InputBar />
+		<div className='bg-zinc-200  flex-grow flex flex-col justify-between h-full overflow-x-hidden pl-[10%] sm:pl-0'>
+			{chatInfo.activeGroup ? (
+				<>
+					<GroupHeader />
+					<div className='flex-grow overflow-y-auto overflow-x-hidden '>
+						{chatInfo.activeGroup.messages.map(
+							(message: Message, index: number) => (
+								<MessageBox message={message} key={index} />
+							)
+						)}
+					</div>
+					<InputBar />
+				</>
+			) : (
+				<NoGroup />
+			)}
 		</div>
 	);
 };
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,14 +6,29 @@ import { Group } from '../../types/types';
 import { GroupBox } from './GroupBox';
 import { JoinGroupBox } from './JoinGroupBox';
 import { CreateNewGroupBox } from './CreateNewGroupBox';
+import { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 export const SideBar = () => {
 	const groups: Group[] = useAppSelector(
 		(state: RootState) => state.chat.groups
 	);
 
+	const handlers = useSwipeable({
+		onSwipedLeft: () => setIsOpen(false),
+		onSwipedRight: () => setIsOpen(true),
+		trackMouse: true,
+	});
+	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<div className='bg-zinc-200 border-r-2 p-2 border-zinc-400  basis-[350px] flex flex-col gap-2 text-white overflow-y-auto h-full shrink-0'>
+		<div
+			{...handlers}
+			className={`bg-zinc-200 border-r-2 p-2 border-zinc-400  basis-[350px] flex flex-col gap-2 text-white overflow-y-auto h-full shrink-0 grow-0 fixed sm:static ${
+				isOpen ? 'translate-x-0 w-full' : '-translate-x-[90%] w-full'
+			} transition-all duration-300 ease-in-out
+			sm:!translate-x-0
+				`}
+		>
 			{groups.map((group: Group) => (
 				<GroupBox group={group} key={group.id} />
 			))}
